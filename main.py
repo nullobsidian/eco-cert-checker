778341bad8d36bc94ae0b3ee9964dce26ca461f1
Moses Marquez <moses.marquez@cartiga.com>
2024-03-25 07:14:28 -0500

    add linting

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import os
-import re
+"""
+This script is part of the eco-cert-checker for ECO DevOps excerise. It provides functionality
+to asynchronously check the validity of SSL/TLS certificates for a list of IP addresses. The 
+checks are performed using the check_certificate function, which should be implemented to
+make the actual verification calls. The module utilizes asyncio to handle concurrent checks 
+efficiently.
+"""
+
 import asyncio
 import datetime
 import ipaddress
+import os
+import re
+import subprocess
 from subprocess import PIPE
-from aiohttp import ClientSession
+
 from aio_statsd import StatsdClient
+from aiohttp import ClientSession
 from aiologger import Logger
 from aiologger.formatters.json import JsonFormatter
-from asyncio.subprocess import create_subprocess_shell
 
 # Config
 SLACK_WEBHOOK_URL = os.getenv(
@@ -39,6 +48,17 @@ logger = Logger.with_default_handlers(level="DEBUG", formatter=JsonFormatter())
 
 
 def get_service_details(ip):
+    """
+    Retrieves the service name and port number for a given IP address based on predefined service
+    configurations.
+
+    Args:
+        ip (str): The IP address for which service details are needed.
+
+    Returns:
+        tuple: A tuple containing the service name (str) and port number (int) if the IP matches a
+               serviceconfiguration, otherwise (None, None).
+    """
     for service, config in SERVICE_CONFIG.items():
         if ipaddress.ip_address(ip) in config["network"]:
             return service, config["port"]
@@ -46,6 +66,20 @@ def get_service_details(ip):
 
 
 async def check_certificate(ip, semaphore):
+    """
+    Asynchronously checks the SSL/TLS certificate of a given IP address by making an OpenSSL
+    connection. Logs various potential issues like connection timeouts, SSL errors, or certificate
+    validity issues.
+
+    Args:
+        ip (str): The IP address to check the certificate for.
+        semaphore (asyncio.Semaphore): Semaphore object to control the concurrency of certificate
+        checks.
+
+    Returns:
+        dict: A dictionary containing the IP, status, and service information related to the 
+              certificate check, or None if an error occurred.
+    """
     service, port = get_service_details(ip)
     if not service:
         await logger.error(
@@ -71,7 +105,9 @@ async def check_certificate(ip, semaphore):
             stdout_output, stderr_output = await process.communicate()
             stdout_output = str(process.stdout) if process.stdout is not None else ""
             stderr_output = str(process.stderr) if process.stderr is not None else ""
-        except Exception as e:
+        except asyncio.CancelledError:
+            pass
+        except (OSError, subprocess.SubprocessError) as e:
             await logger.error({"ip": ip, "port": port, "ERROR": str(e)})
             return {
                 "ip": ip,
@@ -168,6 +204,12 @@ async def check_certificate(ip, semaphore):
 
 
 async def notify_slack(message):
+    """
+    Sends an asynchronous notification to a Slack channel using a predefined webhook URL.
+
+    Args:
+        message (str): The message to send to the Slack channel.
+    """
     try:
         async with ClientSession() as session:
             await asyncio.wait_for(
@@ -178,6 +220,12 @@ async def notify_slack(message):
 
 
 async def update_statsd(metric):
+    """
+    Asynchronously updates a StatsD gauge with a specific metric.
+
+    Args:
+        metric (str): The metric name to update in StatsD.
+    """
     try:
         async with statsd_client:
             await asyncio.wait_for(statsd_client.gauge(metric, 1), timeout=TIMEOUT)
@@ -186,10 +234,15 @@ async def update_statsd(metric):
 
 
 async def main():
+    """
+    The main asynchronous entry function to check SSL/TLS certificates for a list of IP addresses. 
+    It reads IP addresses from a file, checks their certificates concurrently with a controlled 
+    concurrency level, and logs or notifies based on the certificate status.
+    """
     semaphore = asyncio.Semaphore(CONCURRENT_LIMIT)
     tasks = []
 
-    with open(IP_FILE_PATH) as file:
+    with open(IP_FILE_PATH, encoding="utf-8") as file:
         for line in file:
             ip = line.strip()
             tasks.append(check_certificate(ip, semaphore))
